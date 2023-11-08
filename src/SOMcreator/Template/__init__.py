98e2de645b5e3285e44679645c6456cc1241e634
Christoph Mellueh <=>
2023-11-08 15:44:20 +0100

    fix bug that untested.js allways says "Allgemeine_Eigenschaften"

--- src/SOMcreator/Template/__init__.py
+++ src/SOMcreator/Template/__init__.py
@@ -7,6 +7,7 @@ TEMPLATE = "template.txt"
 FAST_TEMPLATE = "fast_template.txt"
 MAPPING_TEMPLATE = "mapping_template.txt"
 BOOKMARK_TEMPLATE = "bookmark_template.txt"
+UNTESTED = "untested_template.txt"
 
 with importlib.resources.open_text("SOMcreator.Template","ifc.json") as file:
     dict = json.load(file)
