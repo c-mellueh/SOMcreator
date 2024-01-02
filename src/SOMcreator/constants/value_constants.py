1ebde59ed030fbd3932133fdb5a5e39b8d12b4df
Christoph Mellueh <=>
2024-01-02 13:35:30 +0100

    rename Datatypes

--- src/SOMcreator/constants/value_constants.py
+++ src/SOMcreator/constants/value_constants.py
@@ -2,16 +2,22 @@ VALUE = "Value"
 FORMAT = "Format"
 RANGE = "Range"
 LIST = "List"
-XS_STRING = "xs:string"
-XS_DOUBLE = "xs:double"
-XS_BOOL = "xs:boolean"
-XS_LONG = "xs:long"
-DATATYPE_NUMBER = "xs:double"
-XS_INT = "xs:int"
+XS_STRING = "IfcLabel"
+XS_DOUBLE = "IfcReal"
+XS_BOOL = "IfcBoolean"
+XS_LONG = "IfcReal"
+DATATYPE_DATE = "IfcDate"
+DATATYPE_NUMBER = "IfcReal"
+XS_INT = "IfcInteger"
 RANGE_STRINGS = ["Range", "range", "RANGE"]
 VALUE_TYPE_LOOKUP = {VALUE: "Value", FORMAT: "Format", RANGE: "Range", LIST: "List",}
 AGGREGATION = 1
 INHERITANCE = 2
 SHARED_PARAMETERS = "SharedParameters"
 EXISTS = "Exists"
-DATA_TYPES = [XS_STRING,XS_LONG,XS_INT,XS_DOUBLE,XS_BOOL]
+DATA_TYPES = [XS_STRING,XS_LONG,XS_INT,XS_DOUBLE,XS_BOOL,DATATYPE_DATE]
+OLD_DATATYPE_DICT = {"xs:string": XS_STRING,
+                     "xs:double": XS_DOUBLE,
+                     "xs:boolean": XS_BOOL,
+                     "xs:long": XS_LONG,
+                     "xs:int": XS_INT}
