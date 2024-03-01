ade402d0271780b6d47641cc57cf455116a21a08
ChristophMellueh <74136980+c-mellueh@users.noreply.github.com>
2024-03-01 12:50:59 +0100

    bump Version

--- src/SOMcreator/__init__.py
+++ src/SOMcreator/__init__.py
@@ -3,5 +3,5 @@ from .external_software import desite, excel, allplan, revit, vestra, card1
 from .constants import json_constants, value_constants
 from .tools import merge_projects
 
-__version__ = "1.6.9"
+__version__ = "1.6.10"
 active_project = None
